07ceda0aceb9afd299d622325944c0c0471827fe
Piotr Kluska <klu@zurich.ibm.com>
2024-06-21 10:24:39 -0700

    fix: update lowering with pt2e (#2)
    fix bug on lowering the model to static quantization.
    check if the node is fx.Node or int

--- qattn/__init__.py
+++ qattn/__init__.py
@@ -3,7 +3,7 @@ from .fx import convert  # noqa: F401
 from . import nn  # noqa: F401
 from .pt2e.dynamo.backend import backends  # noqa: F401
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 
 def version() -> str:
